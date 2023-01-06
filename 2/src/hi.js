bf157d5dccc8f89a7570246c8f9764c80b08aafd
jinsoo <admin@binary01.me>
2023-01-06 22:45:00 +0900

    feat: 2

--- 2/src/hi.js
+++ 2/src/hi.js
@@ -1,6 +1,8 @@
-"use strict";
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var hello_1 = require('./hello');
 var str = 'TS';
 var hi = function () {
-    console.log("Hello ".concat(str, "!"));
+  console.log('Hello '.concat(str, '!'));
 };
-helloFunc;
+(0, hello_1.helloFunc)();
