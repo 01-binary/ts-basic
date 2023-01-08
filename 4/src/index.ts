3b1d92c30dce2a81ddbb559ce58b35205f55f092
jinsoo <admin@binary01.me>
2023-01-08 18:42:03 +0900

    feat: 4

--- 4/src/index.ts
+++ 4/src/index.ts
@@ -1,37 +1,56 @@
-// function add(n1: number, n2: number): number {
-//   return n1 + n2;
-// }
-
-const add = (n1: number, n2: number) => {
+function add(n1: number, n2: number): number {
   return n1 + n2;
-};
+}
+
+// const add = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
 
 const a = add(1, 2);
-const b = add('1', 2);
-const c = add(10, 20, 30);
-const d = add(10);
+const b = add('1', 2); // Error
+const c = add(10, 20, 30); // Error
+const d = add(10); // Error
 
-// function printResult(num: number): void {
-// 	console.log('Result' + num);
-// }
+function printResult(num: number): void {
+  console.log('Result' + num);
+}
 
-// printResult(5);
+printResult(5);
 
-// function add2(n1: number, n2?: number): number {
+function add2(n1: number, n2?: number): number {
+  // ?는 나중에 추가
+  if (!n2) return n1;
+  return n1 + n2;
+}
+
+// function add2(n1?: number, n2: number): number { // 불가
 //   if (!n2) return n1;
 //   return n1 + n2;
 // }
 
-// const a2 = add2(1, 2);
-// const b2 = add2(10, 20, 30);
-// const c2 = add2(10);
-
-// function add3(n1: number, ...nums: number[]): number {
-//   let totalOfNums = 0;
-//   for (let key in nums) {
-//     totalOfNums += nums[key];
-//   }
-//   return n1 + totalOfNums;
+// function add2(n1: number, n2?: number, n3?: number): number {
+//   if (!n2) return n1;
+//   return n1 + n2;
 // }
 
-// const result = add3(10, 20, 30, 40);
+const a2 = add2(1, 2);
+const b2 = add2(10, 20, 30);
+const c2 = add2(10);
+
+function add3(n1: number, n2: number = 0): number {
+  if (!n2) return n1;
+  return n1 + n2;
+}
+
+const a3 = add3(1, 2);
+const c3 = add3(10);
+
+function add4(n1: number, ...nums: number[]): number {
+  let totalOfNums = 0;
+  for (let key in nums) {
+    totalOfNums += nums[key];
+  }
+  return n1 + totalOfNums;
+}
+
+const result = add4(10, 20, 30, 40);
--- 4/src/temp.js
+++ 4/src/temp.js
@@ -1,3 +1,21 @@
 function add(n1, n2) {
   return n1 + n2;
 }
+
+function add2(n1, n2) {
+  if (!n2) return n1;
+  return n1 + n2;
+}
+
+const a2 = add2(1, 2);
+const b2 = add2(10, 20, 30);
+const c2 = add2(10);
+
+function add3(n1, n2 = 0) {
+  if (!n2) return n1;
+  return n1 + n2;
+}
+
+const a3 = add3(1, 2);
+const b3 = add3(10, 20, 30);
+const c3 = add3(10);
